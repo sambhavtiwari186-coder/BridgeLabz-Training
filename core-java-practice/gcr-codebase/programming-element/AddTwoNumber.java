19d0f72df265f181d332509900c3145f773ff487
Anuj Verma <sambhavtiwari71@gmail.com>
2025-12-17 16:29:56 +0530

    [Sambhav Tiwari] Add Two Number

---
+++ core-java-practice/gcr-codebase/programming-element/AddTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AddTwoNumber{
+    public static void main(String[] args) {
+        // Take two input
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int m= sc.nextInt();
+        // Make an output variable 
+        int ans=m+n;
+        // Print the answer
+        System.out.println(ans);
+    }
+}
