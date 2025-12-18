535ab9d6057228a4f5a10af177f6a7e40ea3fa50
Anuj Verma <sambhavtiwari71@gmail.com>
2025-12-18 15:26:54 +0530

    [Sambhav Tiwari] Add Two Numbers

---
+++ core-csharp-practice/gcr-codebase/programming-element/AddTwoNumber.cs
@@ -0,0 +1,13 @@
+using System;
+// Creating a AddTwoNumber Class
+public class AddTwoNumber{
+	public static void Main(){
+		// taking two number as input from user
+		int n = Convert.ToInt32(Console.ReadLine());
+		int m = Convert.ToInt32(Console.ReadLine());
+		// Taking a variable to store the sum
+		int a= n+m;
+		// Printing the desired sum
+		Console.WriteLine(a);
+	}
+}
