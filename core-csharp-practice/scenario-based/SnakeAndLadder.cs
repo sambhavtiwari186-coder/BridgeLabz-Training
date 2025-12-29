018c29ecd0a5d2848867427403624b55feade4f4
Anuj Verma <sambhavtiwari71@gmail.com>
2025-12-29 23:43:18 +0530

    [Sambhav Tiwari] Added Two Sceanrio Based Questions named Snake and Ladder and Temperature Analyser

---
+++ core-csharp-practice/scenario-based/SnakeAndLadder.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+  Summary:-
+  This program implements a console-based Snake 
+  and Ladder game using core C# concepts without using 
+  OOP or collection frameworks. It supports multiple players (2–4) who 
+  take turns rolling a dice and moving on a 100-cell board. 
+  The game applies snake and ladder rules and continues until a 
+  player reaches exactly position 100, 
+  at which point the winner is declared.
+  */
+
+
+namespace BridgeLabzTraining.Scenario_Based
+{
+    internal class SnakeAndLadder
+    {
+
+        //Method to start the Snake and Ladder game
+        public static void SnakeAndLadderImplementation()
+        {
+            //Getting number of players input from the user
+            Console.WriteLine("Enter the number of players between 2 and 4:");
+            int numberOfPlayers = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOfPlayers > 4 && numberOfPlayers < 2)
+            {
+                Console.WriteLine("Number of players is Invalid!");
+            }
+
+            int[] playerArray = new int[numberOfPlayers];
+
+            bool didWin = false;
+
+            //Game loop continues until a player wins
+            while (!didWin)
+            {
+                // Loop for each player's turn
+                for (int i = 0; i < playerArray.Length; i++)
+                {
+                    int diceNumber = RollDice();
+                    Console.WriteLine($"Dice throw: {diceNumber}");
+
+
+                    int oldPosition = playerArray[i];
+                    int newPosition = MovePlayer(playerArray[i], diceNumber);
+
+                    if (oldPosition == newPosition)
+                    {
+                        Console.WriteLine("Cannot move! Next Position exceeds 100");
+                        continue;
+                    }
+
+
+                    int resultingPosition = ApplySnakeAndLadder(newPosition);
+                    Console.WriteLine($"Moving player from {oldPosition} to {resultingPosition}");
+                    playerArray[i] = resultingPosition;
+                    bool winCheck = CanWin(playerArray[i]);
+                    if (winCheck)
+                    {
+                        Console.WriteLine($"Player {i + 1} WINS!");
+                        didWin = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //Method to roll the dice and get a number
+        public static int RollDice()
+        {
+            Random rand = new Random();
+            return rand.Next(1, 7);
+        }
+
+        //Method to move the player from one position to another
+        public static int MovePlayer(int oldPosition, int diceValue)
+        {
+            if ((diceValue + oldPosition) > 100) return oldPosition;
+            return diceValue + oldPosition;
+        }
+
+        //Method to check if the position has a snake or ladder
+        public static int ApplySnakeAndLadder(int newPosition)
+        {
+            int[] snakes = { 99, 89, 63, 24 };
+            int[] snakesDown = { 5, 43, 27, 9 };
+
+            int[] ladders = { 7, 22, 48, 73 };
+            int[] laddersUp = { 38, 52, 78, 94 };
+
+            for (int i = 0; i < snakes.Length; i++)
+            {
+                if (newPosition == snakes[i])
+                {
+                    Console.WriteLine($"Haww! You were cut by a Snake and you will move to {snakesDown[i]}");
+                    return snakesDown[i];
+                }
+            }
+            for (int i = 0; i < ladders.Length; i++)
+            {
+                if (newPosition == ladders[i])
+                {
+                    Console.WriteLine($"Hurray! You climbed a ladder to {laddersUp[i]}");
+                    return laddersUp[i];
+                }
+            }
+            return newPosition;
+        }
+
+        //Method to see if any player wins after a dice roll or not
+        public static bool CanWin(int position)
+        {
+            return position == 100;
+        }
+    }
+}
---
+++ core-csharp-practice/scenario-based/TemperatureAnalyser.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * TempratureAnalyzer is a C# class that simulates and analyzes weekly hourly temperature data using a 7×24 2D array. 
+ * It generates random temperatures between 20°C and 35°C for each hour of the week. 
+ * The class provides methods to identify and display the hottest and coldest temperatures along with their respective days, calculate and display the average temperature for the week, and display day names using a switch-case. 
+ * It demonstrates array manipulation, nested loops, random number generation, and basic data analysis in a modular, non-static structure.
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+namespace BridgeLabzTraining.Senario_based
+{
+    internal class TempratureAnalyzer
+    {
+        // method which will be called by Main
+        public void AnalyzeTemp()
+        {
+            float[,] tempData = new float[7, 24];
+            GetTemperature(tempData);
+            HottestAndColdest(tempData);
+            AvgerageTemp(tempData);
+        }
+        //method to get random temp in our array
+        public void GetTemperature(float[,] tempData)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                //DisplayDay(i);
+                for (int j = 0; j < 24; j++)
+                {
+                    tempData[i, j] = 20f + (float)rnd.NextDouble() * 15f;
+                    //Console.WriteLine($"At this time {j} temp is {tempData[i, j]}");
+                }
+            }
+        }
+        // method to display the day according to the number
+        public void DayDisplayer(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    Console.WriteLine("Monday");
+                    break;
+                case 1:
+                    Console.WriteLine("TuesDay");
+                    break;
+                case 2:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Thrusday");
+                    break;
+                case 4:
+                    Console.WriteLine("Friday");
+                    break;
+                case 5:
+                    Console.WriteLine("Saturaday");
+                    break;
+                case 6:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //method to find hottest and coldest day and print it
+        public void HottestAndColdest(float[,] tempData)
+        {
+            float max = int.MinValue;
+            int maxday = 0;
+            float min = int.MaxValue;
+            int minday = 0;
+
+            for (int i = 0; i < 7; i++)
+            {
+
+                for (int j = 0; j < 24; j++)
+                {
+                    if (tempData[i, j] > max)
+                    {
+                        max = tempData[i, j];
+                        maxday = j;
+                    }
+                }
+            }
+            Console.Write($"Maximum temp is {max} at day ");
+            DayDisplayer(maxday);
+            Console.WriteLine();
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 24; j++)
+                {
+                    if (tempData[i, j] < min)
+                    {
+                        min = tempData[i, j];
+                        minday = j;
+                    }
+                }
+            }
+            Console.Write($"Minmum temp is {min} at day ");
+            DayDisplayer(minday);
+            Console.WriteLine();
+
+        }
+
+        //method to find the average temperature for 7 days
+        public void AvgerageTemp(float[,] tempData)
+        {
+            float total = 0;
+            for (int i = 0; i < 7; i++)
+            {
+
+                for (int j = 0; j < 24; j++)
+                {
+                    total += tempData[i, j];
+                }
+            }
+            float avg = total / (7 * 24);
+
+            Console.WriteLine($"Avg temp accross the week is {avg}");
+        }
+    }
+}
