b87b033fdd7968280a513cb535b305ed1fb05b04
Anuj Verma <sambhavtiwari71@gmail.com>
2026-01-11 22:46:48 +0530

    [Sambhav Tiwari] Added Questions of Sorting-Algorithms

---
+++ dsa-csharp-practice/gcr-codebase/dsa-csharp-sorting-algorithms/BubbleSortStudentMarks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Sorting_Algorithms
+{
+    internal class BubbleSortStudentMarks
+    {
+        static void BubbleSort(int[] marks)
+        {
+            int n = marks.Length;
+            bool swapped;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (marks[j] > marks[j + 1])
+                    {
+                        // Swap
+                        int temp = marks[j];
+                        marks[j] = marks[j + 1];
+                        marks[j + 1] = temp;
+
+                        swapped = true;
+                    }
+                }
+
+                // If no swaps occurred, array is sorted
+                if (!swapped)
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+            int[] studentMarks = new int[10];
+            Console.WriteLine("Enter Student Marks Here :: ");
+            for(int i = 0; i < 10; i++)
+            {
+                studentMarks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Before Sorting:");
+            foreach (int mark in studentMarks)
+                Console.Write(mark + " ");
+
+            BubbleSort(studentMarks);
+
+            Console.WriteLine("\nAfter Sorting (Ascending Order):");
+            foreach (int mark in studentMarks)
+                Console.Write(mark + " ");
+        }
+    }
+}
---
+++ dsa-csharp-practice/gcr-codebase/dsa-csharp-sorting-algorithms/InsertionSortEmployeeId.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Sorting_Algorithms
+{
+    internal class InsertionSortEmployeeId
+    {
+        static void InsertionSort(int[] empIds)
+        {
+            int n = empIds.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                int key = empIds[i];
+                int j = i - 1;
+
+                // Shift elements greater than key to one position ahead
+                while (j >= 0 && empIds[j] > key)
+                {
+                    empIds[j + 1] = empIds[j];
+                    j--;
+                }
+
+                // Insert key at correct position
+                empIds[j + 1] = key;
+            }
+        }
+
+        static void Main()
+        {
+            int[] employeeIds = new int[5];
+            Console.WriteLine("Enter the Employee Id Here");
+            for(int i = 0; i < employeeIds.Length; i++)
+            {
+                employeeIds[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Before Sorting:");
+            foreach (int id in employeeIds)
+                Console.Write(id + " ");
+
+            InsertionSort(employeeIds);
+
+            Console.WriteLine("\nAfter Sorting (Ascending Order):");
+            foreach (int id in employeeIds)
+                Console.Write(id + " ");
+        }
+    }
+}
