cbb00506d53100a112e38ec6448459a616126744
Anuj Verma <sambhavtiwari71@gmail.com>
2026-01-13 11:42:16 +0530

    [Sambhav Tiwari] Added a Scenario Based Question named Metal-factory-pipe

---
+++ oops-csharp-practice/scenario-based/Metal-Factory-Pipe/IRodCuttingStrategy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.scenario_based_dsa.metal_factory_pipe
+{
+    internal interface IRodCuttingStrategy
+    {
+    }
+}
---
+++ oops-csharp-practice/scenario-based/Metal-Factory-Pipe/RodCuttingUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.scenario_based_dsa.metal_factory_pipe
+{ 
+        class RodCuttingUtility : IRodCuttingStrategy
+        {
+            public int GetMaxProfit(int[] prices, int rodLength)
+            {
+                if (rodLength == 0)
+                {
+                    return 0;
+                }
+                int max = int.MinValue;
+                for (int i = 1; i <= rodLength; i++)
+                {
+                    max = Math.Max(max, prices[i - 1] + GetMaxProfit(prices, rodLength - i));
+                }
+                return max;
+            }
+
+            public int GetProfit(int[] prices, int rodLength)
+            {
+                int index = rodLength / 2 - 1;
+                return prices[index] * 2;
+            }
+        }
+
+    }
