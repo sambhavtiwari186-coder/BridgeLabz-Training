6da2ec357a72078ddeb48b73e219a43f95b256c0
Anuj Verma <sambhavtiwari71@gmail.com>
2026-01-13 12:18:31 +0530

    [Sambhav Tiwari] Added a Scenario Based Question named Customer-Furniture-ManuFacturing

---
+++ dsa-csharp-practice/scenario-based/Customer-Furniture-Manufacturing/FurnitureUtility.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.scenario_based_dsa.Customer_Furniture_Manufacturing
+{
+    internal class FurnitureUtility
+    {
+    }
+}
